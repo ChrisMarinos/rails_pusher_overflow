bc216db56b98bc7f74e7ecf8a30f2e6a855cd115
Chris Marinos <cmmarinos@gmail.com>
2012-09-27 14:49:29 -0400

    initial build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPusherOverflow::Application.config.secret_token = '6673fe89d735c790e2c145bc8cf2ee7a6624eced287dfaa10ca7703e5a61201d54ea5195f473cef6f74a58fae31fec5c37e66e0922bd4782e591ec37894aca6c'
